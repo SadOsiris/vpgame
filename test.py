d75ca09601d5c0109a50d2d445bb7d31995d1d4d
Z <xzyxzy1989@gmail.com>
2018-03-11 03:17:55 -0400

    conccurrent pooling tids json, dumping to json_dict

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ import concurrent.futures
 limit= 50
 page=1
 betlist=[]
+myDict={}
 tid=100094194
 
 
@@ -18,7 +19,7 @@ def getjson(page=None,tid=None):
     else:
         url="http://www.vpgame.com/gateway/v1/match/schedule?tid="+str(tid)
     # Fetch url
-    print("Fetching url..")
+    print("Fetching url.. "+ str(url) + '\n')
 
     # Do the HTTP get request
     response = requests.get(url) #http request
@@ -35,23 +36,11 @@ def getjson(page=None,tid=None):
     return data
 
 
-def tester(url):
+def tester(tid):
     # Fetch url
-    print("Fetching url..")
+    myjson=getjson(None,tid)
 
-    # Do the HTTP get request
-    response = requests.get(url) #http request
-
-    # Error handling
-
-    # Check for HTTP codes other than 200
-    if response.status_code != 200:
-        print('Status:', response.status_code, 'Problem with the request. Exiting.')
-        exit()
-
-    # Decode the JSON response into a dictionary and use the data
-    data = response.json()
-    return data
+    myDict[tid]=myjson
 
 def dumpJson(ofile,myList):
     with open(ofile,'w') as outfile:
@@ -74,25 +63,45 @@ def getTidJson(infile,myList=None):
     tid=[x.strip() for x in tid]
     for i in range(len(tid)):
         data=getjson(None,tid[i])
-        myList.append(data['body'])
+        myList[tid]=(data['body'])
         print("dumping "+str(i))
 
 
 
+
+
+def getTidUrls(infile,n):
+    with open(infile) as f:
+        tid=f.readlines()
+    tid = [x.strip() for x in tid]
+    myList=[]
+    for i in range(0,n):
+        myList.append("http://www.vpgame.com/gateway/v1/match/schedule?tid="+tid[i])
+    #print(myList)
+    return myList
+
+
+def getTids(infile,n):
+    with open(infile) as f:
+        tid=f.readlines()
+    tid = [x.strip() for x in tid]
+    return tid[0:n]
+
 def main():
     #data=getjson(None,tid)
     #dumpJson("tid.txt",data)
-    url="http://www.vpgame.com/gateway/v1/match/schedule?tid=100094194"
+    #url="http://www.vpgame.com/gateway/v1/match/schedule?tid=100094194"
 
-    url = "http://www.vpgame.com/gateway/v1/match/schedule?tid=100094194"
+    #url = "http://www.vpgame.com/gateway/v1/match/schedule?tid=100094194"
 
-    urls = [url] * 100
+    #urls = [url] * 100
+    tids=getTids("tid_list.txt",4)
+
     with concurrent.futures.ThreadPoolExecutor(max_workers=100) as pool:
-        results = pool.map(tester, urls)
+        results = pool.map(tester, tids)
 
 
-for x in results:
-        print results
+    print(myDict)
 
 if __name__=="__main__":
     main()
--- vpgame.py
+++ vpgame.py
@@ -1,26 +1,29 @@
 import requests
 import json
+import concurrent.futures
 
-
+from progress.bar import Bar
 
 # Set the request parameters
-limit= 50
-page=1
-betlist=[]
-tid=100094194
+limit = 50
+page = 1
+betlist = {}
+tid = 100094194
+myDict = {}
+totaltid = 0
+bar = Bar('\nProcessing', max=9459)
 
 
-
-def getjson(page=None,tid=None):
-    if page!=None:
-        url="http://www.vpgame.com/gateway/v1/match/?category=dota&status=close&limit="+str(limit)+"&page="+str(page)
+def getjson(page=None, tid=None):
+    if page != None:
+        url = "http://www.vpgame.com/gateway/v1/match/?category=dota&status=close&limit=" + str(limit) + "&page=" + str(
+            page)
     else:
-        url="http://www.vpgame.com/gateway/v1/match/schedule?tid="+str(tid)
+        url = "http://www.vpgame.com/gateway/v1/match/schedule?tid=" + str(tid)
     # Fetch url
-    print("Fetching url..")
 
     # Do the HTTP get request
-    response = requests.get(url) #http request
+    response = requests.get(url)  # http request
 
     # Error handling
 
@@ -33,46 +36,64 @@ def getjson(page=None,tid=None):
     data = response.json()
     return data
 
-def dumpJson(ofile,myList):
-    with open(ofile,'w') as outfile:
-        json.dump(myList,outfile)
 
-def sanitizeJsonBody(myList,page):
+def dumpJson(ofile, myList):
+    with open(ofile, 'w') as outfile:
+        json.dump(myList, outfile)
+
+
+def sanitizeJsonBody(myList, page):
     while True:
-        data=getjson(page)
+        data = getjson(page)
         if not data['body']:
             print(page)
             break
-        for i in range(0,len(data['body'])):
+        for i in range(0, len(data['body'])):
             betlist.append(data['body'][i])
-        page+=1
+        page += 1
 
 
-def getTidJson(infile,myList=None):
+def getTids(infile):
     with open(infile) as f:
-        tid=f.readlines()
-    tid=[x.strip() for x in tid]
-    for i in range(len(tid)):
-        data=getjson(None,tid[i])
-        myList.append(data['body'])
-        print("dumping "+str(i))
+        tid = f.readlines()
+    tid = [x.strip() for x in tid]
+    return tid
 
+
+def worker(tid):
+    myjson = getjson(None, tid)
+    myDict[tid] = myjson
+    bar.next()
+
+
+def concurrentpool():
+    tids = getTids("tid_list.txt")
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as pool:
+        pool.map(worker, tids)
+
+    bar.finish()
+    print(len(myDict))
+    dumpJson("json_dict.txt", myDict)
 
 
 def main():
-    #data=getjson(None,tid)
-    #dumpJson("tid.txt",data)
-    getTidJson("tournament_schedule_id.txt",betlist)
-    dumpJson('tournament.json',betlist)
+    # data=getjson(None,tid)
+    # dumpJson("tid.txt",data)
+    # getTidJson("tid_list.txt",betlist)
+    # print(betlist)
+    # dumpJson('tournament.dict',betlist)
+
+    with open("json_dict.txt", "r") as f:
+        data = json.load(f)
+    print(data['100094194']['body'][0])
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
-#tournamentid
-#tid = data['body'][0]['tournament_schedule_id']
-#print('most recent tid:', tid)
+# tournamentid
+# tid = data['body'][0]['tournament_schedule_id']
+# print('most recent tid:', tid)
 
-#dumpJson("gettid.txt",betlist)
+# dumpJson("gettid.txt",betlist)
 
 # code ends
